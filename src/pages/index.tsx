f22b8e42491c1506cd78721327c4a927b08e7c97
samjkenney <129895815+samjkenney@users.noreply.github.com>
2025-03-07 13:51:00 -0600

    de-templated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,8 +11,8 @@ export default function Home() {
     return (
         <>
             <Head>
-                <title>Phaser Nextjs Template</title>
-                <meta name="description" content="A Phaser 3 Next.js project template that demonstrates Next.js with React communication and uses Vite for bundling." />
+                <title>Build A Banana</title>
+                <meta name="description" content="A Phaser 3 Next.js project that demonstrates Next.js with React communication and uses Vite for bundling." />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.png" />
             </Head>
