8582fb185816e3968d8f2ed97c1f473b68479cfd
Nadya Konadu <nkonadu@macalester.edu>
2025-03-27 19:38:15 -0500

    Button Testing in DressUp

--- src/game/main.ts
+++ src/game/main.ts
@@ -4,7 +4,7 @@ import { DressUp } from './scenes/DressUp';
 import { Peel } from './scenes/Peel';
 import { Split } from './scenes/Split';
 import { AUTO, Game } from 'phaser';
-import { Preloader } from './scenes/Preloader';
+
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
@@ -15,7 +15,6 @@ const config: Phaser.Types.Core.GameConfig = {
     parent: 'game-container',
     backgroundColor: '#028af8',
     scene: [
-        Preloader,
         MainMenu,
         DressUp,
         Split,
--- src/game/scenes/DressUp.ts
+++ src/game/scenes/DressUp.ts
@@ -1,6 +1,9 @@
 import { GameObjects, Scene } from 'phaser';
 
 export class DressUp extends Scene {
+
+    private selectedFace: Phaser.GameObjects.Image | null = null; 
+
     constructor() {
         super('DressUp');
     }
@@ -9,85 +12,171 @@ export class DressUp extends Scene {
         this.load.image('dressUpBackground', 'assets/1.png');  
         this.load.image('menu', 'assets/Menu.png');  
         this.load.image('banana', 'assets/Banana.png'); 
-        this.load.image('buttonOne', 'assets/Button1.png');  
-        this.load.image('buttonTwo', 'assets/Button2.png');  
-        this.load.image('buttonThree', 'assets/Button3.png'); 
         this.load.image('face', 'assets/Face.png');     
         this.load.image('glasses', 'assets/Glasses.png'); 
         this.load.image('shirt', 'assets/Shirt.png'); 
 
-        this.load.image('faceChill', 'assets/FaceChill.png'); 
-        this.load.image('hatClown', 'assets/HatClown.png');  
-        this.load.image('bodyClown', 'assets/BodyClown.png'); 
+        //Face Options
+        this.load.image('prettyFace', 'assets/Pretty Icon.png'); 
+        this.load.image('minionFace', 'assets/Minion Icon.png'); 
+
+
+        this.load.image('Face1', 'assets/face trixie.png'); 
+        this.load.image('Face2', 'assets/face minion.png'); 
+
+
+
+       
         this.load.image('nextButton', 'assets/nextButton.png');
+
+      
         
     }
 
     create() {
+
+        
         this.add.image(512, 384,'dressUpBackground'); 
         const menu = this.add.image(800, 404, 'menu');  
         menu.setScale(1.7);
         const banana = this.add.image(210, 384, 'banana'); 
         banana.setScale(0.4);  
-           
-        const buttonOne = this.add.image(750, 270, 'buttonOne').setVisible(false);
-        const buttonTwo = this.add.image(750, 270, 'buttonTwo').setVisible(false);
-        const buttonThree = this.add.image(750, 270, 'buttonThree').setVisible(true);
+        
+        // Dress Up Options
+        const prettyFace = this.add.image(710, 300, 'prettyFace').setInteractive().setVisible(false);
+        prettyFace.setScale(0.4);
+        const minionFace = this.add.image(910, 300, 'minionFace').setInteractive().setVisible(false);
+        minionFace.setScale(0.4);
+
    
-        const face = this.add.image(6, 334, 'face'); 
+  
+
+
+        // Categories
+   
+        //Face
+        const face = this.add.image(595, 254, 'face'); 
+        face.setScale(0.15);
         face.setInteractive();
-        const glasses =this.add.image(625, 434, 'glasses'); 
-        glasses.setInteractive();
-        const shirt = this.add.image(615, 504, 'shirt'); 
-        shirt.setInteractive();
 
-
-        const faceChill = this.add.image(210, 384, 'faceChill').setVisible(false).setInteractive();
-        const hatClown = this.add.image(210, 384, 'hatClown').setVisible(false).setInteractive();
-        const bodyClown = this.add.image(210, 384, 'bodyClown').setVisible(false).setInteractive();
+       
+        face.on('pointerout', () => {
+            // face.clearTint(); 
+        });
 
         
+        face.on('pointerover', () => {
+            face.setTint(0xffb6c1); 
+        });
+
+        face.on('pointerdown', () => {
+            face.setTint(0xff69b4); 
+            prettyFace.setVisible(true);
+            minionFace.setVisible(true);
 
 
-         face.on('pointerdown', () => {
-            buttonOne.setVisible(false);  
-            buttonTwo.setVisible(false); 
-            buttonThree.setVisible(true); 
+            prettyFace.on('pointerover', () => {
+                if (this.selectedFace !== prettyFace) { 
+                    prettyFace.setTint(0xffb6c1); 
+                }
+            });
+        
+            prettyFace.on('pointerout', () => {
+                if (this.selectedFace !== prettyFace) { 
+                    prettyFace.clearTint();
+                }
+            });
+        
+            prettyFace.on('pointerdown', () => {
+                this.selectedFace = prettyFace;
+                prettyFace.setTint(0xff69b4); 
+                minionFace.clearTint(); 
+                console.log('pretty face selected');
+            });
+        
+            minionFace.on('pointerover', () => {
+                if (this.selectedFace !== minionFace) { 
+                    minionFace.setTint(0xffb6c1); 
+                }
+            });
+        
+            minionFace.on('pointerout', () => {
+                if (this.selectedFace !== minionFace) { 
+                    minionFace.clearTint();
+                }
+            });
+        
+            minionFace.on('pointerdown', () => {
+                this.selectedFace = minionFace; 
+                minionFace.setTint(0xff69b4); 
+                prettyFace.clearTint(); 
+                console.log('minion face selected');
+            });
+        });
+
+
+        // Glasses
+        const glasses = this.add.image(592, 354, 'glasses'); 
+        glasses.setScale(0.15);
+        glasses.setInteractive();
+
+      
+        glasses.on('pointerout', () => {
+            glasses.clearTint(); 
+        });
+
+        glasses.on('pointerover', () => {
+            glasses.setTint(0xffb6c1); 
         });
 
        
         glasses.on('pointerdown', () => {
-            buttonOne.setVisible(true); 
-            buttonTwo.setVisible(false);  
-            buttonThree.setVisible(false); 
+            glasses.setTint(0xff69b4); 
         });
 
-        
+        // Shirt
+        const shirt = this.add.image(595, 454, 'shirt'); 
+        shirt.setScale(0.15);
+        shirt.setInteractive();
+
+        shirt.on('pointerout', () => {
+            shirt.clearTint();
+        });
+
+        shirt.on('pointerover', () => {
+            shirt.setTint(0xffb6c1); 
+        });
+
         shirt.on('pointerdown', () => {
-            buttonOne.setVisible(false); 
-            buttonTwo.setVisible(true); 
-            buttonThree.setVisible(false);  
+            shirt.setTint(0xff69b4); 
         });
 
        
-    buttonOne.on('pointerdown', () => {
-        faceChill.setVisible(true);  
-        hatClown.setVisible(false); 
-        bodyClown.setVisible(false);
-    });
-
+  
     
 
 
-    const nextButton = this.add.sprite(800, 654, 'nextButton').setInteractive(); //centered???
-    nextButton.setScale(0.1);
-
+        //Next Button 
+        const nextButton = this.add.sprite(800, 654, 'nextButton').setInteractive();
+        nextButton.setScale(0.1); 
        
-    nextButton.on('pointerdown', () => {
-        this.scene.start('Peel'); 
-    });
 
-     
+        // Mouse hovers 
+        nextButton.on('pointerover', () => {
+            nextButton.setTint(0xffb6c1); 
+            nextButton.setScale(0.12); 
+        });
+
+        // Mouse leaves 
+        nextButton.on('pointerout', () => {
+            nextButton.clearTint();
+            nextButton.setScale(0.1); 
+        });
+
+        // When clicked
+        nextButton.on('pointerdown', () => {
+            this.scene.start('Peel'); 
+        });
 
 
 
