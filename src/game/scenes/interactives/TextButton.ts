666a865d41ec45b41f3b4a4149c67a88d6928e1a
Zander Leong <zleong@macalester.edu>
2025-04-07 22:15:07 -0700

    implement TextButton

--- src/game/scenes/interactives/TextButton.ts
+++ src/game/scenes/interactives/TextButton.ts
@@ -3,10 +3,12 @@ import { GameObjects } from "phaser";
 import { Button } from './Button';
 
 export class TextButton extends Button{
-    constructor(scene: Scene, x: number, y: number, width: number, height: number, color: number, displayText: string, textStyle: GameObjects.Text, action: Function){
+    constructor(scene: Scene, x: number, y: number, width: number, height: number, color: number, displayText: string, textStyle: GameObjects.TextStyle, action: Function){
         super(scene, x, y, width, height, color, action);
 
-        var text = new GameObjects.Text(scene, width / 2, height / 2, displayText, textStyle); //figure out text styling, CSS
+        var text = new GameObjects.Text(scene, 0, 0, displayText, textStyle); //figure out text styling, CSS
+        text.setOrigin(0.5);
+        text.setPosition(width / 2, height / 2);
         this.scaleToButton(text);
 
         this.add(text);
