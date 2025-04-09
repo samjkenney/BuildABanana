7ea4bef72c9f895a96f3871c0a15c693887ac45e
Zander Leong <zleong@macalester.edu>
2025-04-09 12:11:33 -0700

    add SceneButton, update NextButton,

---
+++ src/game/scenes/interactives/SceneButton.ts
@@ -0,0 +1,19 @@
+import { Scene } from "phaser";
+import { GameObjects } from "phaser";
+import { TextButton } from "./TextButton";
+import { TextStyles } from '../toolbox/TextStyles';
+
+export class SceneButton extends TextButton{
+    //private static color: string; //make constant?
+    //private static width: number; //make constant?
+    //private static height: number; //make constant?
+
+    constructor(scene: Scene, x: number, y: number, width: number, height: number, color: number, displayText: string, textStyle: GameObjects.TextStyle, scaleToButtonSize: boolean, scaleOnHover: boolean, nextScene: string,){
+        var action = () => {
+            scene?.scene.start(nextScene);
+        };
+
+        super(scene, x, y, width, height, color, displayText, textStyle, scaleToButtonSize, scaleOnHover, action);
+
+    }
+}
