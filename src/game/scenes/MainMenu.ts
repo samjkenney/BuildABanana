cd78963f97a8df4f5dff07bb89e8520a221c7269
Zander Leong <zleong@macalester.edu>
2025-04-09 14:42:11 -0700

    implement getter functions for TextStyles

--- src/game/scenes/MainMenu.ts
+++ src/game/scenes/MainMenu.ts
@@ -14,18 +14,16 @@ export class MainMenu extends Scene {
     }
 
     create() {
-        var titleStyle = new GameObjects.Text(this, 0, 0 , "title", TextStyles.title)
-
         var background = this.add.image(0, 0, 'backgroundImage');
         background.setOrigin(0);
         
-        var startButton = new TextButton(this, background.width / 2 - 125, background.height / 2 - 50, 250, 100, 0xF9B1B4, "Start", titleStyle.style, false, true, () => {this.scene.start('Name')});
+        var startButton = new TextButton(this, background.width / 2 - 125, background.height / 2 - 50, 250, 100, 0xF9B1B4, "Start", TextStyles.getTitleStyle(this), false, true, () => {this.scene.start('Name')});
         var startButton2 = new NextButton(this, background.width / 2 - 100, background.height / 2 - 40 + 240, "Name");
 
-        //const startButton = this.add.sprite(this.scale.displaySize.width, this.scale.displaySize.height, 'startButton').setInteractive(); //centered???
         this.add.existing(startButton).setInteractive();
         this.add.existing(startButton2).setInteractive();
 
+        //const startButton = this.add.sprite(this.scale.displaySize.width, this.scale.displaySize.height, 'startButton').setInteractive(); //centered???
         // this.startButton.on('pointerdown', () => {
         //     this.scene.start('Name'); 
         // });
--- src/game/scenes/toolbox/TextStyles.ts
+++ src/game/scenes/toolbox/TextStyles.ts
@@ -1,13 +1,55 @@
+import { GameObjects, Scene } from 'phaser';
+
 export class TextStyles{
     static title = {
-        fontFamily: "Kitto",
-        color: "#ffffff",
-        fontSize: 80
-    };
-    
+            fontFamily: "Kitto",
+            color: "#ffffff",
+            fontSize: 80
+        };
+        
     static button = {
         fontFamily: "Kitto",
         color: "#ffffff",
         fontSize: 60
+    }
+
+    // static styleMap: { [key: string]: object } = {
+    //     "title": {
+    //         fontFamily: "Kitto",
+    //         color: "#ffffff",
+    //         fontSize: 80
+    //     },
+
+    //     "button": {
+    //         fontFamily: "Kitto",
+    //     color: "#ffffff",
+    //     fontSize: 60
+    //     }
+    // };
+
+    // static styleMap1 = new Map<string, object>([
+    //     ["title", {
+    //         fontFamily: "Kitto",
+    //         color: "#ffffff",
+    //         fontSize: 80
+    //     }],
+
+    //     ["button", {
+    //         fontFamily: "Kitto",
+    //     color: "#ffffff",
+    //     fontSize: 60
+    //     }]
+    // ]);
+
+    // static getStyle(scene: Scene, styleType: string){
+    //     return new GameObjects.Text(scene, 0, 0, "", TextStyles.styleMap1.get(styleType)).style;
+    // }
+
+    static getTitleStyle(scene: Scene){
+        return new GameObjects.Text(scene, 0, 0, "", TextStyles.title).style;
+    }
+
+    static getButtonStyle(scene: Scene){
+        return new GameObjects.Text(scene, 0, 0, "", TextStyles.button).style;
     }
 }
