a9e7b207e5c767ffbd336f9057f403c91f92fdde
samjkenney <129895815+samjkenney@users.noreply.github.com>
2025-05-05 11:00:59 -0500

    implementing eulogy message

--- src/game/scenes/Eulogy.ts
+++ src/game/scenes/Eulogy.ts
@@ -13,14 +13,11 @@ export class Eulogy extends Scene {
         //load background
         this.load.image("backgroundT", "assets/Basic_BKG.jpeg"); 
     }
-    //TODO: add images?? 
-    //TODO: fix spacing
-    //TODO: implement personality/aspiration
-    //TODO: font style?
+    
     create(){
         const name : String = this.registry.get("banana").getName();
-       // const personality : Characteristic = this.registry.get("banana").getPersonality();
-       // const aspiration : Characteristic = this.registry.get("banana").getAspiration();
+        const personality : Characteristic = this.registry.get("banana").getPersonality();
+       const aspiration : Characteristic = this.registry.get("banana").getAspiration();
         
 
         const bg = this.add.image(849, 567.5,'backgroundT').setScale(1.5); //replace w custom background
@@ -29,8 +26,8 @@ export class Eulogy extends Scene {
          "Who was loved by all; \n" +
           "a devoted friend, child, and sibling. \n" +
          "They were a banana of many talents. \n" +
-         name + "personality.getEulogyEntry()" +  "\n" +
-         name + "aspiration.getEulogyEntry()" +  "\n" +
+         name + " " + personality.getEulogyEntry() +  "\n" +
+         name + " " + aspiration.getEulogyEntry() +  "\n" +
          "They will be missed by all who knew them. \n" +
             "May they rest in peace.\n" +
             "\n \n \n \n \n" +
@@ -55,12 +52,12 @@ export class Eulogy extends Scene {
         this.tweens.add({
             targets: titleText,
             y: { from: 1000, to: -5000 },  // Move from bottom to top 
-            duration: 20000,           // Animation duration (in milliseconds)
+            duration: 50000,           // Animation duration (in milliseconds)
             ease: 'Linear',           // Animation easing function
             repeat: 0,              
             yoyo: false          
           });
 
-        
+      //TODO: add a button to go back to the main menu  
     }
 }
